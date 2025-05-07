cfbaf386e64a626a726178b4b4e79361f344d247
Ramprasanth7119 <ramprasanth2802@gmail.com>
2025-05-07 21:26:38 +0530

    Initial setup was added

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const dotenv = require('dotenv').config();
+
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors()); 
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.log(err));
+
+
+app.get('/' , (req,res) =>{
+    res.json({message : "Connected to the server"});
+})
+
+app.listen(PORT , (req,res) =>{
+    console.log(`App is listening at ${PORT}`);
+})
