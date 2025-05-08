5e244ff57f4e61322eea5cbb919f554e2b0545c4
Ramprasanth7119 <ramprasanth2802@gmail.com>
2025-05-08 13:57:12 +0530

    sample

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const dotenv = require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 
+console.log("hello");
+
 app.use(cors()); 
 app.use(helmet());
 app.use(cookieParser());
